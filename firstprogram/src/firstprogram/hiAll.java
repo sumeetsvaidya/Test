a7ef0f4d2f2edcbb64bf30cb13edbe01c8254713
sumeet <sumeet@sumeet-K56CA>
2021-01-17 18:56:27 +0530

    First commit. firstprogram

---
+++ firstprogram/src/firstprogram/hiAll.java
@@ -0,0 +1,9 @@
+package firstprogram;
+
+public class hiAll {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys");
+		String myPath = System.getProperty("user.dir");
+		System.out.println("Hello guys my project path is:" + myPath);
+	}
+}
