a11d7334c37197546111edf23f65ffe957d13fd1
sumeet <sumeet@sumeet-K56CA>
2021-01-20 20:17:23 +0530

    created anotherProj

--- anotherProj/src/test/java/anotherProj/TestOne.java
+++ anotherProj/src/test/java/anotherProj/TestOne.java
@@ -1,4 +1,4 @@
-package mavenOne;
+package anotherProj;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -7,26 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class test {
+public class TestOne {
 	static ChromeOptions options = new ChromeOptions();
 	static String projectPath = System.getProperty("user.dir");
-	static WebDriver driver = null;
-	
-	
+	static WebDriver driver = null;	
 	public static void main(String[] args) {
-		
-	
+
 		//System.setProperty("webdriver.chrome.driver",projectPath + "/Drivers/Chrome/chromedriver"); //For Chrome
 		System.setProperty("webdriver.gecko.driver",projectPath + "/Drivers/Gecko/geckodriver"); //For Gecko
 		options.setAcceptInsecureCerts(true);
-		
+
 		//driver = new ChromeDriver(options); //For chrome driver
 		driver = new FirefoxDriver(); //For Gecko driver
-		
+
 		driver.get("http://google.com");
 		driver.findElement(By.name("q")).sendKeys("automation");
 		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
-		
+
 		driver.close();
 	}
 }
+
+
+
